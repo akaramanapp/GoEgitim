87767a4ef1946a801acf8631a17f5a46ff26e3d4
Abdulkerim <abdulkerimkaraman@gmail.com>
2022-03-13 16:36:27 +0300

    add function example

--- functions/functions.go
+++ functions/functions.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
+// iki parametre alan bir function tanimi
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
 
+	// functionun cagirilmasi
+	fmt.Println(add(42, 13))
 }
