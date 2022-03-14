e7b1080d6433fda9499db063706c0da833c1bd6f
Abdulkerim <abdulkerimkaraman@gmail.com>
2022-03-14 09:01:21 +0300

    add method in http project

--- http/http.go
+++ http/http.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+var Articles []Article
+
+// create type article struct
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home page")
 }
 
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	Articles = []Article{
+		{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+
+	json.NewEncoder(w).Encode(Articles)
+}
+
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
 }
 
 func main() {
